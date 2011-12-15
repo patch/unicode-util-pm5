f39776aecf878f90af6e0ef04a10a0f78350c5aa
Nick Patch <patch@cpan.org>
2011-12-15 07:13:51 -0500

    new project

---
+++ lib/Unicode/Util.pm
@@ -0,0 +1,46 @@
+package Unicode::Util;
+
+use 5.006;
+use strict;
+use warnings;
+use parent 'Exporter';
+use Carp;
+
+our $VERSION   = '0.01';
+our @EXPORT    = qw( );
+our @EXPORT_OK = qw( );
+
+1;
+
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Unicode::Util - A selection of useful Unicode grapheme-aware functions
+
+=head1 VERSION
+
+This document describes Unicode::Util version 0.01.
+
+=head1 SYNOPSIS
+
+    ...
+
+=head1 DESCRIPTION
+
+...
+
+=head1 AUTHOR
+
+Nick Patch <patch@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2011 Nick Patch
+
+This library is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself.
+
+=cut
