3ecb1a6acf90df416a7f3bf0882c6d6620b99081
Nick Patch <patch@cpan.org>
2011-12-15 08:01:26 -0500

    add length functions

--- lib/Unicode/Util.pm
+++ lib/Unicode/Util.pm
@@ -1,14 +1,36 @@
 package Unicode::Util;
 
-use 5.006;
+use 5.008;
 use strict;
 use warnings;
 use parent 'Exporter';
-use Carp;
+use Encode qw( encode_utf8 );
+use Unicode::Normalize;
 
 our $VERSION   = '0.01';
-our @EXPORT    = qw( );
-our @EXPORT_OK = qw( );
+our @EXPORT_OK = qw(
+    graph_length
+    code_length
+    byte_length
+);
+
+sub graph_length {
+    my ($str) = @_;
+    utf8::upgrade($str);
+    return scalar( () = $str =~ m/\X/g );
+}
+
+sub code_length {
+    my ($str) = @_;
+    utf8::upgrade($str);
+    return length $str;
+}
+
+sub byte_length {
+    my ($str) = @_;
+    utf8::upgrade($str);
+    return length encode_utf8( NFKC($str) );
+}
 
 1;
 
@@ -30,7 +52,19 @@ This document describes Unicode::Util version 0.01.
 
 =head1 DESCRIPTION
 
-...
+=over
+
+=item graph_length
+
+=item code_length
+
+=item byte_length
+
+=back
+
+=head1 TODO
+
+graph_reverse graph_chop graph_split graph_substr code_substr byte_substr graph_index code_index byte_index graph_rindex code_rindex byte_rindex
 
 =head1 AUTHOR
 
