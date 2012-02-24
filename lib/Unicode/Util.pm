b722a19f934eb1d4d78b9eaa772e92876567da49
Nick Patch <patch@cpan.org>
2012-02-23 23:54:08 -0500

    add optional encoding argument for byte_length
    Added optional second argument to byte_length to specify the encoding, with
    the default remaining UTF-8.

--- lib/Unicode/Util.pm
+++ lib/Unicode/Util.pm
@@ -5,7 +5,7 @@ use strict;
 use warnings;
 use utf8;
 use parent 'Exporter';
-use Encode qw( encode_utf8 );
+use Encode qw( encode find_encoding );
 
 our $VERSION   = '0.03_1';
 our @EXPORT_OK = qw(
@@ -14,6 +14,8 @@ our @EXPORT_OK = qw(
     graph_reverse
 );
 our %EXPORT_TAGS = ( all => \@EXPORT_OK );
+
+use constant DEFAULT_ENCODING => 'UTF-8';
 
 sub graph_length {
     my ($str) = @_;
@@ -28,9 +30,14 @@ sub code_length {
 }
 
 sub byte_length {
-    my ($str) = @_;
+    my ($str, $enc) = @_;
     utf8::upgrade($str);
-    return length encode_utf8($str);
+
+    if ( !$enc || !find_encoding($enc) ) {
+        $enc = DEFAULT_ENCODING;
+    }
+
+    return length encode($enc, $str);
 }
 
 sub graph_chop {
@@ -121,8 +128,10 @@ count in a string.
 
 =item byte_length($string)
 
-Returns the length in bytes of the given string encoded as UTF-8.  This is the
-number of bytes that many computers would count when storing a string.
+=item byte_length($string, $encoding)
+
+Returns the length in bytes of the given string if it were encoded using the
+specified encoding or UTF-8 if no encoding is supplied.
 
 =back
 
--- t/length.t
+++ t/length.t
@@ -1,7 +1,7 @@
 use strict;
 use warnings;
 use utf8;
-use Test::More tests => 6;
+use Test::More tests => 9;
 use Unicode::Util qw( graph_length code_length byte_length );
 
 my $grapheme = "\x{44E}\x{301}";  # ю́
@@ -15,3 +15,9 @@ my $ascii = 'abc';
 is graph_length($ascii), 3, 'graphemes in ASCII string';
 is code_length($ascii),  3, 'codepoints in ASCII string';
 is byte_length($ascii),  3, 'bytes in ASCII string';
+
+my $aring = 'å';
+
+is byte_length($aring),               2, 'letter a with ring in UTF-8 (default)';
+is byte_length($aring, 'UTF-8'),      2, 'letter a with ring in UTF-8';
+is byte_length($aring, 'ISO-8859-1'), 1, 'letter a with ring in ISO-8859-1';
