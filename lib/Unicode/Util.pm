36dc20912a02417065b93fe623d635de2aa54df5
Nick Patch <patch@cpan.org>
2012-02-23 22:38:55 -0500

    bump dev version

--- lib/Unicode/Util.pm
+++ lib/Unicode/Util.pm
@@ -7,7 +7,7 @@ use utf8;
 use parent 'Exporter';
 use Encode qw( encode_utf8 );
 
-our $VERSION   = '0.03';
+our $VERSION   = '0.03_1';
 our @EXPORT_OK = qw(
     graph_length  code_length  byte_length
     graph_chop    code_chop
@@ -69,7 +69,7 @@ Unicode::Util - Unicode-aware versions of built-in Perl functions
 
 =head1 VERSION
 
-This document describes Unicode::Util version 0.03.
+This document describes Unicode::Util version 0.03_1.
 
 =head1 SYNOPSIS
 
